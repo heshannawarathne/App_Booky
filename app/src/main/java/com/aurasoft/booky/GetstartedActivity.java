af02d013df5e29d889ca7934d75cfbb18b56942e
Heshan Nawarathna <117019222+heshannawarathne@users.noreply.github.com>
2026-03-11 21:20:58 +0530

    today_first

---
+++ app/src/main/java/com/aurasoft/booky/GetstartedActivity.java
@@ -0,0 +1,27 @@
+package com.aurasoft.booky;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class GetstartedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(
+                android.view.WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                android.view.WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        );
+
+
+        setContentView(R.layout.activity_getstarted);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/aurasoft/booky/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.aurasoft.booky;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+       TextView tv= findViewById(R.id.login_to_sighUp);
+
+       tv.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent i=new Intent(LoginActivity.this,SignupActivity.class);
+               startActivity(i);
+               finish();
+           }
+
+       });
+
+
+
+    }
+}
